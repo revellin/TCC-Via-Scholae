83a4c4bba0fea004e1e7a4dc5555bcde6ae4159d
cooingcy <matheuscurciromano@gmail.com>
2024-09-04 21:21:11 -0300

    atualizando

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,48 @@
-import React from "react";
-import { View, Text } from 'react-native'
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { CustomTitleText } from '../../components';
 
 export const HomeScreen = () => {
-    return (
-      <View>
-        <Text>Home Screen</Text>
+  return (
+    <View style={styles.container}>
+      <View style={styles.router}>
+        <CustomTitleText style={styles.routertitle}>Rota</CustomTitleText>
       </View>
-    );
-  };
+      <View style={styles.timer}>
+        <View style={styles.line}>
+          <View styles={styles.end}>
+            <Text>Chegada</Text>
+            <Text>00:00</Text>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  router: {
+    width: '100%',
+    height: 648,
+    alignItems: 'center',
+    backgroundColor: '#262626',
+  },
+  routertitle: {
+    color: '#ffffff',
+    padding: 170,
+  },
+  timer: {
+    flex: 3,
+    flexDirection: 'row',
+  },
+  line: {
+    height: 5, //Espressura da linha
+    width: 10,
+    backgroundColor: "#E9B224", //Cor da linha
+  }
+
+})
